182170e3298655a3dbef915f08b678d05dffbc0e
Shardul Nalegave <shardul.nalegave.2005@gmail.com>
2024-06-17 20:28:49 +0530

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import MillionLint from '@million/lint';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+const _plugins = [react()];
+_plugins.unshift(MillionLint.vite())
+export default defineConfig(async () => ({
+  plugins: _plugins,
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"]
+    }
+  }
+}));
