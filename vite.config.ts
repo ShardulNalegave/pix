c97f5ec45265d10a74db99f1e9eac11bd6fb6d46
Shardul Nalegave <shardul.nalegave.2005@gmail.com>
2024-06-17 22:37:04 +0530

    Removed excess template code, added tanstack-router, tailwindcss, shadcn-ui and few other changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import MillionLint from '@million/lint';
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
-// https://vitejs.dev/config/
-const _plugins = [react()];
-_plugins.unshift(MillionLint.vite())
 export default defineConfig(async () => ({
-  plugins: _plugins,
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
+  plugins: [
+    MillionLint.vite(),
+    react(),
+    TanStackRouterVite(),
+  ],
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
     watch: {
-      // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"]
     }
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 }));
